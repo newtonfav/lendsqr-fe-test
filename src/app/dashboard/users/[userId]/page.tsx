118ca9e4b3732c3ab60370eb82cded5596dd8fdc
Favour Oghenekowho <gh.oghene@gmail.com>
2024-10-30 15:55:09 +0100

    details page

--- src/app/dashboard/users/[userId]/page.tsx
+++ src/app/dashboard/users/[userId]/page.tsx
@@ -1,4 +1,7 @@
 import BackArrow from "@/src/app/components/icons/BackArrow";
+import ProfileIcon from "@/src/app/components/icons/ProfileIcon";
+import Star from "@/src/app/components/icons/Star";
+import UnStar from "@/src/app/components/icons/UnStar";
 import Link from "next/link";
 import React from "react";
 
@@ -16,9 +19,39 @@ export default function UserPage() {
         <h1>User Details</h1>
 
         <div className="userpage__heading--buttons">
-          <button className="button button--blacklist">Blacklist User</button>
-          <button className="button button--activate">Activate User</button>
+          <button className="button--blacklist">Blacklist User</button>
+          <button className="button--activate">Activate User</button>
         </div>
+      </div>
+
+      <div className="userpage__container">
+        <div className="userpage__details">
+          <div className="userpage__details--profileicon">
+            <ProfileIcon />
+          </div>
+
+          <div className="userpage__details--profile">
+            <h1 className="userpage__details--name">Grace Effiom</h1>
+            <span className="userpage__details--userid">LSQFf587g90</span>
+          </div>
+
+          <div className="userpage__details--usertier">
+            <p>User's Tier</p>
+            <span>
+              <Star />
+              {Array.from([1, 2]).map((index) => (
+                <UnStar key={index} />
+              ))}
+            </span>
+          </div>
+
+          <div className="userpage__details--balance">
+            <h3 className="">₦200,000.00</h3>
+            <span>9912345678/Providus Bank</span>
+          </div>
+        </div>
+
+        <div>section</div>
       </div>
     </div>
   );
