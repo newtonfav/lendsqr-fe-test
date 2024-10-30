3923839e67d76d4f255c2715fce5a3e2df33946e
Favour Oghenekowho <gh.oghene@gmail.com>
2024-10-30 13:46:47 +0100

    started user details page

--- src/app/dashboard/users/[userId]/page.tsx
+++ src/app/dashboard/users/[userId]/page.tsx
@@ -1,5 +1,25 @@
+import BackArrow from "@/src/app/components/icons/BackArrow";
+import Link from "next/link";
 import React from "react";
 
-export default function page() {
-  return <div>page</div>;
+export default function UserPage() {
+  return (
+    <div className="userpage">
+      <Link href="/dashboard/users">
+        <div className="userpage__back">
+          <BackArrow />
+          <span>Back to Users</span>
+        </div>
+      </Link>
+
+      <div className="userpage__heading">
+        <h1>User Details</h1>
+
+        <div className="userpage__heading--buttons">
+          <button className="button button--blacklist">Blacklist User</button>
+          <button className="button button--activate">Activate User</button>
+        </div>
+      </div>
+    </div>
+  );
 }
