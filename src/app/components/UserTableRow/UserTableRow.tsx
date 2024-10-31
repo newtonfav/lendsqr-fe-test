632bfc037af4d11258de78be7b2b4b61970eba52
Favour Oghenekowho <gh.oghene@gmail.com>
2024-10-31 10:13:30 +0100

    added tablerow menu

--- src/app/components/UserTableRow/UserTableRow.tsx
+++ src/app/components/UserTableRow/UserTableRow.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import ThreeDots from "../icons/ThreeDots";
 import formatDate from "../../utils/functions/formatDate";
 import { getStatusFromNumericValue } from "../../utils/functions/getUserFieldFromNumericValue";
 import Link from "next/link";
+import Blacklist from "../icons/Blacklist";
+import Activate from "../icons/Activate";
+import View from "../icons/View";
 
 interface IUserRow {
   organisation: string;
@@ -26,11 +30,34 @@ export default function UserTableRow({
   userId,
 }: IUserRow) {
   const userStatus = getStatusFromNumericValue(status);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const toggleMenuVisibility = () => {
+    setIsMenuVisible((prev) => !prev);
+  };
+
+  // Close the menu when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuVisible(false);
+      }
+    };
+
+    // Add event listener for clicks on the document
+    document.addEventListener("mousedown", handleClickOutside);
+
+    //cleanup function
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="tablerow">
       <Link href={`users/${userId}`} className="tablerow__organisation">
-        <span className="">{organisation}</span>
+        <span>{organisation}</span>
       </Link>
       <Link href={`users/${userId}`} className="tablerow__username">
         <span>{`${firstName} ${lastName}`}</span>
@@ -41,9 +68,29 @@ export default function UserTableRow({
       <span className={`tablerow__status tablerow__status--${userStatus}`}>
         {userStatus}
       </span>
-      <span className="tablerow__dots">
+
+      {/* Toggle menu visibility on click */}
+      <span className="tablerow__dots" onClick={toggleMenuVisibility}>
         <ThreeDots />
       </span>
+
+      {/* Conditionally render the menu based on isMenuVisible */}
+      {isMenuVisible && (
+        <div ref={menuRef} className="tablerow__menu">
+          <Link href={`users/${userId}`} className="tablerow__menu--view">
+            <View />
+            View Details
+          </Link>
+          <span className="tablerow__menu--blacklist">
+            <Blacklist />
+            Blacklist User
+          </span>
+          <span className="tablerow__menu--activate">
+            <Activate />
+            Activate User
+          </span>
+        </div>
+      )}
     </div>
   );
 }
